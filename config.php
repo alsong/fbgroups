ca57f306761926e878321d5a8f724f7caa907580
Oburusule Dunstan <alsongdunstan2@gmail.com>
2019-09-14 10:34:41 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 	$appId = '381360626133280'; 
 	$appSecret = '7dfccb057dea3b78c9b84ae4f699a1f1';
 	$requiredPermissions = 'public_profile, publish_actions, user_groups'; 
-	$redirectURL = 'https://fbgroupz.herokuapp.com/post.php'; // FB will redirect to this page with a code
+	$redirectURL = 'https://fbgroupz.herokuapp.com'; // FB will redirect to this page with a code
  	
  	$minDelayTime = 20; // Set the min delay in seconds between api requests
 	$maxDelayTime = 40; // Set the max delay in seconds between api requests
